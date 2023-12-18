f18939bebdb8a888cdd0bf520ed806368613cf50
Vuong Chi Hao <haowangbright@gmail.com>
2023-12-18 23:15:17 +0700

    chore: modulation option to Unet

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ from ddpm.denoising_diffusion_pytorch import Unet, GaussianDiffusion, Trainer
 model = Unet(
     dim = 64,
     dim_mults = (1, 2, 4, 8),
-    flash_attn = True
+    flash_attn = True,
+    use_modulation = True
 )
 
 clip_model, _ = clip.load('RN50', device='cpu')
