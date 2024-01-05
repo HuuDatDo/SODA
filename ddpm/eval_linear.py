0c3c58781bfab80d05b05ac2be414f9a75b739ad
ihatecfishes <your@email.com>
2024-01-05 22:51:24 +0000

    fix: utils bug

--- ddpm/eval_linear.py
+++ ddpm/eval_linear.py
@@ -24,7 +24,7 @@ from torchvision import datasets
 from torchvision import transforms as pth_transforms
 from torchvision import models as torchvision_models
 
-import utils
+from . import utils
 # import vision_transformer as vits
 
 
